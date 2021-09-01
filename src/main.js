283107322aeb038d1e245080a714668fbfe7559b
Kiel Moulon <sha2q@mtmail.mtsu.edu>
2021-08-31 20:17:58 -0500

    hi

--- src/main.js
+++ src/main.js
@@ -3,5 +3,5 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import titleMixin from './mixins/titleMixin'
-
+console.log('dddd')
 createApp(App).use(store).use(router).mixin(titleMixin).mount("#app");
